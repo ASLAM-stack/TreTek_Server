d5a3158690a596dc69ee45e7dc4ddac9c6e6904f
ASLAM-stack <arifhossainaslam6@gmail.com>
2024-04-30 10:35:32 +0600

    add vercel

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
     const spotCollection = client.db("touristSpotDB").collection("touristSpot");
     const countryCollection = client.db("touristSpotDB").collection("countryList");
